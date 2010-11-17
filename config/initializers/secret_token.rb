fdb1a61fdba38aee0c5e08b67c8ce431ad24f559
Paul Heinemann <junkemailaddress3@gmail.com>
2010-11-17 18:54:01 -0500

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrueDemo::Application.config.secret_token = 'fb752ebcf7f935bf872ce6269066f44171a3873a948c4236240f6fbef3192470c44ceab2c52d5e8e6a28213ad1927b6a3bde556edcfaad71c70ba7d394a43e5e'
